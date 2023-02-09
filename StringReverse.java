159ed580c8c644d19022c9850c19ff79f1bee281
xyz-mail1 <91377933+xyz-mail1@users.noreply.github.com>
2023-02-09 13:37:55 +0530

    Update StringReverse.java

--- StringReverse.java
+++ StringReverse.java
@@ -2,23 +2,22 @@ class StringReverse
 {
 	public static void main(String[] args)
 	{
-		String str = new String("I like boobs");
+		String str = new String("I like Java");
 		System.out.println("Given string: " + str);
 		System.out.println("Reverse string: " + rev(str));
 	}
 	
-	static String rev(String str)    // reverse string
-	{
+	static String rev(String str)   
 		int len = str.length();
-		byte bA[] = new byte[len];
-		bA = str.getBytes(); 
+		byte byteAry[] = new byte[len];
+		byteAry = str.getBytes(); 
 		
-		for (int i=0, j=len-1; i<len/2; i++, j--) // wtf
+		for (int i=0, j=len-1; i<len/2; i++, j--) 
 		{
-			byte tmp = bA[i];
-			bA[i]=bA[j];
-			bA[j]=tmp;
+			byte tmp = byteAry[i];
+			byteAry[i]=byteAry[j];
+			byteAry[j]=tmp;
 		}
-	return new String(bA); // used constructor
+	return new String(byteAry); 
 	}
 }
